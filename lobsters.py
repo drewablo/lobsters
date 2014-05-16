c2b97b549370e227723ab92b02b7c65eec4f7f05
drewablo <andrewt77@gmail.com>
2014-05-15 22:16:06 -0500

    Progress, but not there yet
    I can't get this to iterate through the links on individual lawmakers pages.

--- lobsters.py
+++ lobsters.py
@@ -3,6 +3,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys  
 from bs4 import BeautifulSoup
 from selenium.webdriver.support.ui import Select
+import re
 
 driver = webdriver.Firefox()
 driver.get("http://www.ilsos.gov/lobbyistsearch/")
@@ -15,11 +16,39 @@ select.select_by_visible_text("2014")
 element = driver.find_element_by_xpath("//input[@value='Submit']")
 element.click()
 html_source = driver.page_source
+soup = BeautifulSoup(html_source)
+table = soup.find('table', cellpadding='3')
+x=0
+y=1
+if table.findAll('tr')[x:]:
+	x+=1
+	if soup.findAll('a',href=True):
+		element = driver.find_element_by_class_name("db10ub")
+		element.click()
+		elements = driver.find_elements_by_class_name("db10ub")
+		html_source = driver.page_source
+		soup = BeautifulSoup(html_source)
+		table = soup.find('table', cellpadding='3')
+		while y < len(elements):
+			if soup.findAll('a',href=True):
+				if soup.find("tr")[y:]:
+					y+=1
+					print y
+					if soup.findAll('a',href=True):
+						element = driver.find_element_by_class_name("db10ub")
+						element.click()
+						html_source = driver.page_source
+						soup = BeautifulSoup(html_source)
+						if soup.find('table', id='tabTwo'):
+							element = driver.find_element_by_xpath("//td/font/a")
+							element.click()
+			driver.back();
 
+		'''
+		if table.findAll('tr')[5:]:
+			if soup.findAll('a',href=True):
+				print soup.findAll('a',href=True)
+				element = driver.find_element_by_xpath("//tr/tdfont/a")
+				element.click()
+				'''
 
-soup = BeautifulSoup(html_source, 'html.parser')
-table = soup.find('tablesorter', cellpadding=3)
-for item in table.findAll('tr')[0:]:
-	col = item.findAll('td')
-	name = col[0].string
-	print name
